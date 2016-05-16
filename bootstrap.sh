d9cc95a8867d1d010524df2e7a9bc2e60274b594
Michael Voropaiev <m.voropaiev@gmail.com>
2016-05-16 20:53:13 +0300

    add basic os detection

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,26 @@
 rails_version='4.2.6'
 app_name='webapp'
 
+# should fix osx files ownership (?)
+case $OSTYPE in
+  linux* )
+    user_id="$(id -u)"
+    group_id="$(id -u)"
+    ;;
+  darwin* )
+    user_id="1000"
+    group_id="$(id -u)"
+    ;;
+  * )
+    printf "Unsupported operating system."
+    exit 1
+    ;;
+esac
+
 docker run \
   -it \
   --rm \
-  --user "$(id -u):$(id -g)" \
+  --user "$user_id:$group_id" \
   --volume "$PWD":/usr/src/app \
   -w /usr/src/app \
   rails:$rails_version \
